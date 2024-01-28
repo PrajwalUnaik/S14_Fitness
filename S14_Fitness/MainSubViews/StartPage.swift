cf850f5845a555107074f3cf6de7d678047deb92
prajwalnaik28@gmail.com <prajwalnaik28@gmail.com>
2024-01-28 18:03:15 +0530

    Start page added

---
+++ S14_Fitness/MainSubViews/StartPage.swift
@@ -0,0 +1,18 @@
+//
+//  StartPage.swift
+//  S14_Fitness
+//
+//  Created by Prajwal U on 28/01/24.
+//
+
+import SwiftUI
+
+struct StartPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StartPage()
+}
